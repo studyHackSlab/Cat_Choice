744f46f0b5d7b4822fd36a6024b262c74614ebcb
studyHackSlab <138351574+studyHackSlab@users.noreply.github.com>
2024-09-15 10:38:31 +0900

    Add files via upload

--- cat_choice/js/script.js
+++ cat_choice/js/script.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // console.log(split[split.length - 1].split("_")[this.length + 1].split(".")[0].split("0")[1]);
     // console.log(split[split.length - 1].split("_"));
 
-    const max_number = 3;
+    const max_number = 6;
     // let random_number = Math.floor(Math.random() * max_number) + 1;
     // img[2].src = "img/cat_0" + random_number + ".png";
 
     for (let i = 1; i < img.length; i++) {
         img[i].src = "img/cat_0" + random() + ".png";
-        img[i].src = "img/img/cat_0" + random() + ".jpg";
+        // img[i].src = "img/img/cat_0" + random() + ".jpg";
     }
 
     for (let i = 0; i < li.length; i++) {
@@ -27,6 +27,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return Math.floor(Math.random() * max_number) + 1;
     }
 
+
+    window.setTimeout(function () {
+        let img_black = document.getElementsByTagName("img");
+
+        for (let i = 0; i < img_black.length; i++) {
+            img_black[i].style.display = "none";
+            li[i].classList.add("border");
+            li[i].style.pointerEvents = "auto";
+            // console.log(li[i]);
+        }
+        console.log("5000");
+    }, 5000);
+
+
     window.setTimeout(function () {
         let img_black = document.getElementsByTagName("img");
 
@@ -34,20 +48,34 @@ document.addEventListener('DOMContentLoaded', (event) => {
             // console.log(img_black[i].src);
             let answer = img_black[i].src.split("/")[split.length - 1].split("_")[this.length + 1].split(".")[0].split("0")[1];
             // console.log(img_black[i].src.split("/")[split.length - 1].split("_")[this.length + 1].split(".")[0].split("0")[1]);
-            console.log(img_black[i].src);
-            if (answer == 1 || answer == 3 ) {
-                // if (answer == 3) {
+            // console.log(img_black[i].src);
+            // if (answer == 1 || answer == 3 ) {
+            if (answer == 3 || answer == 6) {
                 li[i].classList.add("answer");
             }
+            else {
+                li[i].classList.add("events_none");
+            }
+
+            if (li[i].className.match("choice answer")) {
+                li[i].classList.remove("choice");
+                li[i].classList.remove("answer");
+                li[i].classList.add("correct_answer");
+            }
+            li[i].style.pointerEvents = "none";
+            li[i].classList.remove("border");
+            img_black[i].style.display = "block";
         }
-        console.log(document.getElementsByClassName("choice answer").length);
-        console.log(document.getElementsByClassName("choice").length)
+        // console.log(document.getElementsByClassName("choice answer").length);
+        // console.log(document.getElementsByClassName("choice").length)
         document.getElementsByTagName("p")[0].style.color = "#000000";
         document.getElementsByTagName("p")[0].style.fontWeight = "900";
-        document.getElementsByTagName("p")[0].innerHTML = "正解数：" + document.getElementsByClassName("choice answer").length;
-        console.log(document.getElementsByTagName("p")[0].innerHTML);
+        // document.getElementsByTagName("p")[0].innerHTML = "正解数：" + document.getElementsByClassName("choice answer").length;
+        document.getElementsByTagName("p")[0].innerHTML = "正解数：" + document.getElementsByClassName("correct_answer").length;
+        // console.log(document.getElementsByTagName("p")[0].innerHTML);
         // alert('時間切れです');
-    }, 5000);
+        console.log("10000");
+    }, 10000);
     // <li><img src="img/cat_01.png" alt="ねこ１"></li>
 
 });
